af20cbf04f844c5f01e52246a0c906b7fdc4fae1
YangZhiXiong <641484973@qq.com>
2024-06-02 23:45:06 +0800

    init backend program

---
+++ src/main/java/com/clouddisk/ClouddiskApplication.java
@@ -0,0 +1,18 @@
+package com.clouddisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CloudDisk应用
+ *
+ * @author CHIIIPLOW
+ * @date 2024/06/02
+ */
+@SpringBootApplication
+public class ClouddiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClouddiskApplication.class);
+    }
+}
