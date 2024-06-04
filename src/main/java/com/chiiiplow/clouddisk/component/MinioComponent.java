5093e3250042ad69845775e908f6c1a7502d424b
YangZhiXiong <641484973@qq.com>
2024-06-04 11:56:20 +0800

    Add MinIO Dependency

---
+++ src/main/java/com/chiiiplow/clouddisk/component/MinioComponent.java
@@ -0,0 +1,11 @@
+package com.chiiiplow.clouddisk.component;
+
+import io.minio.MinioClient;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MinioComponent {
+
+    private MinioClient minioClient;
+
+}
